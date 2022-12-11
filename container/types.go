259403bf05c4e4e4792d7177486eb489ef18f285
Oliver D'Alton <odalton@ipsi.com.au>
2022-12-12 10:16:25 +1100

    Added latest container funcs to GlobalContainer

--- container/types.go
+++ container/types.go
@@ -2,7 +2,6 @@ package container
 
 import (
 	"github.com/binarycurious/light-container/config"
-	"github.com/binarycurious/light-container/telemetry"
 )
 
 type routineKey struct {
@@ -28,29 +27,4 @@ func (gs *GlobalState) NewState(settings *config.Settings) *GlobalState {
 	gs.intitialized = true
 
 	return gs
-}
-
-// Container - defines a simple container interface for executing tasks in a standard way with access to a global state
-type Container interface {
-	GetState() GlobalState
-	GetLogger() telemetry.Logger
-	AddRoutine(key *string, routine *ContainerRoutine) routineKey
-	Execute(key *routineKey)
-	Subscribe(key *routineKey) <-chan *interface{}
-	SendToChannel(key *routineKey, msg *interface{})
-	GetRoutineKey(key *string) routineKey
-}
-
-// ContainerRoutine - defines the routine that is executed by the container
-type ContainerRoutine interface {
-	Execute(routineKey routineKey, container *Container)
-}
-
-// GlobalContainer :  is an implementation of a Global Container
-type GlobalContainer struct {
-	logger   telemetry.Logger
-	state    GlobalState
-	routines map[string]ContainerRoutine
-	inChans  map[string]chan *interface{}
-	outChans map[string]chan *interface{}
 }
