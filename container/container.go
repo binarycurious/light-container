b916714eeb35adc9fca568dcf92435f57e3854fd
Oliver D'Alton <odalton@ipsi.com.au>
2022-12-14 15:02:26 +1100

    more tidy

--- container/container.go
+++ container/container.go
@@ -29,38 +29,3 @@ type Context interface {
 type ContainerRoutine interface {
 	Execute(routineKey *routineKey, container Context)
 }
-
-// Execute : impl of container Execute function
-// func (c *GlobalContainer) Execute(key *routineKey) {
-// 	go (*c.routines[key.key]).Execute(*key, &Context(c))
-// }
-
-// AddRoutine : impl of add routine functions for GlobalContainer (will modify routineName if there is a conflict)
-// func (c *GlobalContainer) AddRoutine(routineName *string, routine *ContainerRoutine) routineKey {
-// 	if len(c.routines) == 0 {
-// 		c.routines = make(map[string]ContainerRoutine, 10)
-// 	}
-// 	rKey := c.GetRoutineKey(routineName)
-// 	_, retry := c.routines[rKey.key]
-
-// 	retries := 0
-// 	for retry {
-// 		retries++
-// 		*routineName = fmt.Sprintf("%s_%d", *routineName, retries)
-// 		rKey = c.GetRoutineKey(routineName)
-// 		_, retry = c.routines[rKey.key]
-// 	}
-
-// 	c.routines[rKey.key] = *routine
-// 	return rKey
-// }
-
-// GetState : impl of get state function for GlobalContainer
-// func (c *GlobalContainer) GetState() *GlobalState {
-// 	return
-// }
-
-// // GetLogger : impl of get logger function for GlobalContainer
-// func (c *GlobalContainer) GetLogger() *telemetry.Logger {
-// 	return &c.logger
-// }
