569e37fffd26495dc6975144464de062e5270bfe
Oliver D'Alton <odalton@ipsi.com.au>
2023-02-13 21:46:40 +1100

    Added log level string matching

--- telemetry/logger.go
+++ telemetry/logger.go
@@ -1,5 +1,7 @@
 package telemetry
 
+import "strings"
+
 // LogLevel type for logging
 type LogLevel byte
 
@@ -15,6 +17,24 @@ const (
 	//LogLevelNone - 4
 	LogLevelNone
 )
+
+// GetLogLevel - returns the loglevel for the given string
+func GetLogLevel(loglvl string) LogLevel {
+	switch strings.ToLower(loglvl) {
+	case "debug":
+		return LogLevelDebug
+	case "info":
+		return LogLevelInfo
+	case "warn":
+		return LogLevelWarn
+	case "error":
+		return LogLevelError
+	case "none":
+		return LogLevelNone
+	default:
+		return LogLevelInfo
+	}
+}
 
 // Logger interface for generic app logging
 type Logger interface {
